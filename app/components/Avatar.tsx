6e25ba8a1b2d6dfcf5e1c8d33d4159103203e236
kaikaci12 <somewonesomewone@gmail.com>
2024-09-09 16:19:04 +0400

    add Login and Signup MenuItems and add toggling funcionality

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+
+function Avatar() {
+  return (
+    <div>
+      <Image
+        className="rounded-full"
+        height="30"
+        width="30"
+        alt="Avatar"
+        src="/images/placeholder.jpg"
+      />
+    </div>
+  );
+}
+
+export default Avatar;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,5 +1,12 @@
 "use client";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useState } from "react";
+
+import MenuItem from "./MenuItem";
 export default function UserMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3 ">
@@ -9,7 +16,28 @@ export default function UserMenu() {
         >
           Airbnb Your Home
         </div>
+        <div
+          onClick={() => {
+            setIsOpen((prev) => !prev);
+          }}
+          className="p-4 md:py-1 md:px-4 border-[1px] border-neutral-200 flex flex-row items-center  gap-3 rounded-full  cursor-pointer hover:shadow-md transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
       </div>
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[15vw] bg-white overflow-hidden top-12 right-0 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign up" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
