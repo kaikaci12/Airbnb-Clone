22560efd5a2efc346201fb207e3730952bf8395a
kaikaci12 <somewonesomewone@gmail.com>
2024-09-21 22:37:36 +0400

    add HeartButton UI for favorite listings

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { UserType } from "@/models/User";
 import React from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 interface HeartButtonProps {
   listingId?: string;
   currentUser?: UserType | null;
@@ -11,5 +12,19 @@ export default function HeartButton({
 }: HeartButtonProps) {
   const hasFavorited = false;
   const toggleFavorite = () => {};
-  return <div className=""></div>;
+  return (
+    <div
+      className="relative hover:opacity-80 transition cursor-pointer"
+      onClick={toggleFavorite}
+    >
+      <AiOutlineHeart
+        size={29}
+        className="fill-white absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </div>
+  );
 }
