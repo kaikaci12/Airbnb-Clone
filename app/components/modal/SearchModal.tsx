ea9b301e2a937863e38940467d64f808f0dc02d2
kaikaci12 <somewonesomewone@gmail.com>
2024-10-09 21:50:41 +0400

    add select location STEP on SearchModel

--- app/components/modal/SearchModal.tsx
+++ app/components/modal/SearchModal.tsx
@@ -1,16 +1,132 @@
-import React from "react";
+"use client";
+import queryString from "query-string";
+import formatISO from "date-fns/formatISO";
+import React, { useCallback, useMemo, useState } from "react";
 import Modal from "./Modal";
 import { useSearchModal } from "@/app/hooks/useSearchModal";
-
+import { useRouter, useSearchParams } from "next/navigation";
+import { Range } from "react-date-range";
+import dynamic from "next/dynamic";
+import { CountrySelectValue } from "../inputs/CountrySelect";
+import Heading from "../Heading";
+import CountrySelect from "../inputs/CountrySelect";
+enum STEPS {
+  LOCATION = 0,
+  DATE = 1,
+  INFO = 2,
+}
 const SearchModal = () => {
   const searchModal = useSearchModal();
+  const router = useRouter();
+  const params = useSearchParams();
+  const [step, setStep] = useState(STEPS.LOCATION);
+  const [location, setLocation] = useState<CountrySelectValue>();
+  const [guestCount, setGuestCount] = useState(1);
+  const [roomCount, setRoomCount] = useState(1);
+  const [bathroomCount, setBathroomCount] = useState(1);
+  const [dateRange, setDateRange] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+  const onSubmit = useCallback(async () => {
+    if (step !== STEPS.INFO) {
+      return onNext();
+    }
+
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+    const updatedQuery: any = {
+      ...currentQuery,
+      locationValue: location?.value,
+      guestCount,
+      roomCount,
+      bathroomCount,
+    };
+    if (dateRange.startDate) {
+      updatedQuery.startDate = formatISO(dateRange.startDate);
+    }
+
+    if (dateRange.endDate) {
+      updatedQuery.endDate = formatISO(dateRange.endDate);
+    }
+    const url = queryString.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    setStep(STEPS.LOCATION);
+    searchModal.onClose();
+
+    router.push(url);
+  }, [
+    step,
+    searchModal,
+    location,
+    router,
+    guestCount,
+    roomCount,
+    bathroomCount,
+    dateRange,
+    onNext,
+    params,
+  ]);
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.INFO) {
+      return "Search";
+    }
+
+    return "Next";
+  }, [step]);
+  const secondActionLabel = useMemo(() => {
+    if (step === STEPS.LOCATION) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Where do you wanna go?"
+        subtitle="Find the perfect location!"
+      />
+      <CountrySelect
+        value={location}
+        onChange={(value) => setLocation(value as CountrySelectValue)}
+      />
+      <hr />
+      <Map center={location?.latlng} />
+    </div>
+  );
   return (
     <Modal
       isOpen={searchModal.isOpen}
       onClose={searchModal.onClose}
       onSubmit={searchModal.onOpen}
       title="Filters"
-      actionLabel="Search"
+      actionLabel={actionLabel}
+      body={bodyContent}
     />
   );
 };
